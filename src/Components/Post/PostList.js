c2419e39b5dffe1062f56f71941ba38f3a27bd58
whataLIN <kitschh@naver.com>
2023-07-17 16:58:23 +0900

    update, delete

--- src/Components/Post/PostList.js
+++ src/Components/Post/PostList.js
@@ -1,74 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import PostList from './Components/PostList';
+import './App.css';
 
-const PostList = () => {
-  const [posts, setPosts] = useState([]);
-  const [newPost, setNewPost] = useState({ title: '', content: '' });
-
-  // 게시글 목록 불러오기
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  const fetchPosts = async () => {
-    try {
-      const response = await axios.get('/api/posts');
-      setPosts(response.data);
-    } catch (error) {
-      console.error('Error fetching posts:', error);
-    }
-  };
-
-  // 새로운 게시글 생성
-  const createPost = async () => {
-    try {
-      const response = await axios.post('/api/posts', newPost);
-      setPosts([...posts, response.data]);
-      setNewPost({ title: '', content: '' });
-    } catch (error) {
-      console.error('Error creating post:', error);
-    }
-  };
-
-  // 게시글 삭제
-  const deletePost = async (postId) => {
-    try {
-      await axios.delete(`/api/posts/${postId}`);
-      setPosts(posts.filter((post) => post.pid !== postId));
-    } catch (error) {
-      console.error('Error deleting post:', error);
-    }
-  };
+const App = () => {
 
   return (
-    <div>
-      <h1>Post List</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Title"
-          value={newPost.title}
-          onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder="Content"
-          value={newPost.content}
-          onChange={(e) => setNewPost({ ...newPost, content: e.target.value })}
-        />
-        <button onClick={createPost}>Create</button>
-      </div>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.pid}>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
-            <button onClick={() => deletePost(post.pid)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <PostList />
     </div>
   );
-};
+}
 
-export default PostList;
+export default App;
