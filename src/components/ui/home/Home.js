3220e249baa0bad03a68c4b50d934b6c88250dbc
SeoSangwon <tkd8973@gmail.com>
2023-07-19 17:09:11 +0900

    edit axios API

--- src/components/ui/home/Home.js
+++ src/components/ui/home/Home.js
@@ -3,7 +3,13 @@ import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
-import Calender from '../../calendar/Calendar';
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import Calendar from '../../calendar/Calendar';
+import ToDoList from '../../todolist/TodoList'
+import rootReducer from '../../../store/rootReducer';
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -25,7 +31,10 @@ function Home() {
         <Grid container spacing={2}>
             <Grid item xs={6} md={8}>
             <Item>
-              캘린더
+              <Provider store={store}>
+                <Calendar></Calendar>
+                <ToDoList></ToDoList>
+              </Provider>
             </Item>
             </Grid>
             <Grid item xs={6} md={4}>
