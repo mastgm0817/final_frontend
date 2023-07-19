901cda9d195d2695a197a6a01dc5ead8fee76db8
rhquddus <rhquddus@naver.com>
2023-07-19 15:37:33 +0900

    delete unused import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import PostList from './components/Post/PostList';
-import UserList from './components/User/UserList';
-=======
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
->>>>>>> 9e43bbf82e9a0c4cbf11ffd9e2b6b48b920a94ea
 
 ReactDOM.render(
     <BrowserRouter>
