c2edd0a5c908d245f089e8d1a6ad602ac77a81e5
rhquddus <rhquddus@naver.com>
2023-07-19 12:37:42 +0900

    src error

--- src/Components/UI/Nav/Nav.js
+++ src/Components/UI/Nav/Nav.js
@@ -12,7 +12,6 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import { useNavigate } from 'react-router-dom';
 
 const pages = ['Home', 'Board', 'Test'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
