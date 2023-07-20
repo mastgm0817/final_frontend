fed014de8ab92188f67be8935d5c096325d6cb35
whataLIN <kitschh@naver.com>
2023-07-20 10:10:23 +0900

    0720-start

--- src/components/post/PostDetail.js
+++ src/components/post/PostDetail.js
@@ -3,10 +3,26 @@ import './PostList.css';
 
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Handlerecommendations from './api/Handlerecommendations';
+import FetchPosts from './api/FetchPosts';
+
 
 const PostDetail = ({ post }) => {
 
-    // const [newComment, setcomment]
+    const HandlerecommendButton = async (post) => {
+        
+        try {
+          await Handlerecommendations(post.pid);
+          const updatedPosts = await FetchPosts();
+        //   setPosts(updatedPosts);
+        } catch(error) {
+          console.error("Error increasing view count:", error);
+        }
+        FetchPosts();
+      };
+
+
     return (
         <Box sx={{align:"center"}}>
             <div style={{marginLeft:"30"}}>
@@ -18,24 +34,15 @@ const PostDetail = ({ post }) => {
                 <tr></tr>
                 <tr></tr>
                 <tr><td><p style={{ marginBottom: '1em' }}>{post.content}</p></td></tr>
+                <tr><td><Button onClick={(event) => {event.stopPropagation(); HandlerecommendButton(post.pid)}}>추천</Button></td></tr>
                 {/* <tr><td><p style={{ wordBreak: 'break-all' }}>{post.comments}</p></td></tr> */}
                 </tbody>
             </table>
             </div>
 
-             <h4>댓글 목록</h4>
-            {/*<div>{post.comments}</div>
 
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'left' }}>
-                <TextField
-                    id="outlined-basic"
-                    label="내용"
-                    variant="standard"
-                    type="text"
-                    value={post.comments}
-                    onChange={e => setNewPost({ ...newPost, comments: e.target.value })}
-                />
-            </div> */}
+             <h4>댓글 목록</h4>
+
         </Box>
     );
 };  
--- src/components/post/api/IncreaseViewCount.js
+++ src/components/post/api/IncreaseViewCount.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const IncreaseViewCount = async (pid) => {
+const IncreaseViewCount = async (postId) => {
+  const postIdString = postId.toString();
     try {
-      const response = await axios.get(`/api/posts/${pid}`, {
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        }
-    });
+      const response = await axios.get(`/api/posts/${postIdString}`);
       return response.data;
     } catch (error) {
       console.error('Failed to increase view count:', error);
