7a7a862c8e7df6c1faf9e4bfb979d677eb2b2f86
rhquddus <rhquddus@naver.com>
2023-07-20 17:36:12 +0900

    monday

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
             <Route path="/users" element={<UserList />} />
           </Routes>
           <Box sx={{ bgcolor: '#F8F6F4', height: '7vh' }} >
-            footer123
+            footer1234
           </Box>
         </Container>
       </Container>
