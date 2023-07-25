15b00de41d78f5cf65f78c1fdd5c60e66f8dc961
rhquddus <rhquddus@naver.com>
2023-07-26 02:25:12 +0900

    social login v1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import ResponsiveAppBar from './nav_bar/nav.client'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ResponsiveAppBar />
+        {children}
+      </body>
     </html>
   )
 }
