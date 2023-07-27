19f94a07fa0b947804d4316661ccf8580a5f3911
whataLIN <kitschh@naver.com>
2023-07-27 09:30:44 +0900

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,12 +72,12 @@ export default function Home() {
           </p>
         </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/board"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          target="_self"
+          rel="noopener noreferrer">
+            
           <h2 className={`mb-3 text-2xl font-semibold`}>
             게시판{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -87,7 +87,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             게시판으로 들어가는 곳
           </p>
-        </a>
+        </Link>
 
       </div>
     </main>
---
+++ src/pages/board.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+import {
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Paper,
+  } from '@mui/material'
+  import '../app/globals.css';
+  import Grid from '@mui/material/Grid';
+  import Box from '@mui/material/Box';
+
+export default function BoardList() {
+
+    
+
+    return (
+        <div>
+            <main className="__className_20951f">
+
+                <div>오.</div>
+                
+                <Grid container>
+                    <Grid item xs={8}>오웅</Grid>
+                </Grid>
+
+            </main>
+        </div>
+    )
+
+}
