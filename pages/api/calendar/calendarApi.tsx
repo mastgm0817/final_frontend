7280d6e9ac927519dd67eb1e6fcca1722f54ccbd
jaiwon880 <rabbitmarry@gmail.com>
2023-08-02 15:52:07 +0900

    move css file

--- pages/api/calendar/calendarApi.tsx
+++ pages/api/calendar/calendarApi.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:8080/calendar";
-
 export const createSchedule = async (nickName: string, requestDTO: any) => {
   const response = await axios.post(`${API_URL}/${nickName}`, requestDTO);
   return response.data;
