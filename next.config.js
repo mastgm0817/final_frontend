794acc1df045ab9581723dff414ea5c4c2edae03
rhquddus <rhquddus@naver.com>
2023-08-04 02:12:23 +0900

    cicd

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  output: "standalone",
+  output: "export",
   env: {
     GOOGLE_ID: process.env.GOOGLE_ID,
     GOOGLE_SECRET: process.env.GOOGLE_SECRET,
