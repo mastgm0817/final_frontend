119543c92e9adaebac46274119056fb2ec696613
whataLIN <kitschh@naver.com>
2023-08-06 17:42:17 +0900

    pagenation

--- app/api/board/SendData.tsx
+++ app/api/board/SendData.tsx
@@ -3,14 +3,16 @@ import axios from "axios";
 const headers = {
     'Content-Type': 'application/json; charset=utf-8'
 };
+// const API_URL = process.env.NEXT_PUBLIC_URL;
+const API_URL = 'http://localhost:8080';
+
 
 const SendData = async (method:string, sendurl:string, data:any, msg:string) => {
-    const targetURL="http://localhost:8080"+sendurl;
-    // console.log(data.nickName);
-    // console.log(data.cContent);
+    const targetURL=API_URL+sendurl;
     try {
         const response = await axios({method,url:targetURL,data,headers});
         console.log(method, response.data);
+        return response.data;
     } catch (error) {
         console.error('Error:', error);
         throw error;
--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import Board from "../board";
+import Board from "../../types/board";
 import WriteBoard from "../../components/WriteBoard";
 import SendData from "../api/board/SendData";
-import Link from "next/link";
-// import Page from "./[pagenation]/page";
+import Page from "./[pagenum]/pagenum";
 
 const defaultBoard: Board = {
   bid: 0,
@@ -21,24 +19,23 @@ const defaultBoard: Board = {
 };
 function Logined(props: any): any {
 
-  const param=1;
-  
   const { data: session } = useSession();
-  // const [boards, setBoards] = useState<Board[]>([]); //board목록
-  const [AddFormClass, setAddFormClass] = useState<String | null>(null); //글추가폼의 class
+  const [boards, setBoards] = useState<Board[] | any>([]); //board목록
+  const [AddFormClass, setAddFormClass] = useState<string | null>(null); //글추가폼의 class
   const [newBoard, CreateNewBoard] = useState<Board>({ ...defaultBoard }); //새로운 board
+  const [pages, setPages] = useState<number>(0)
   
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await SendData("GET", `/api/boards/page/0`,null,"fetch boards");
-  //     setBoards(response);
-  //   } catch (error) {
-  //     console.error("Error fetching boards:", error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  const fetchData = async () => {
+    try {
+      const response = await SendData("GET", `/api/boards/page/0`,null,"fetch boards");
+      setBoards(response);
+    } catch (error) {
+      console.error("Error fetching boards:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   function ToggleAddForm(): any {
     if (AddFormClass === "formOn") {
@@ -55,12 +52,12 @@ function Logined(props: any): any {
     setAddFormClass("formOff");
   }
   
-  // async function CreateBoard(newBoard: Board) {
-  //   newBoard.nickName = `${session ? session.user?.name : null}`;
-  //   await SendData("POST", `/api/boards`,newBoard,"create");
-  //   ToggleAddForm();
-  //   fetchData();
-  // }
+  async function CreateBoard(newBoard: Board) {
+    newBoard.nickName = `${session ? session.user?.name : null}`;
+    await SendData("POST", `/api/boards`,newBoard,"create");
+    ToggleAddForm();
+    fetchData();
+  }
 
 
   if (session) {
@@ -68,7 +65,7 @@ function Logined(props: any): any {
     return session ? (
       
       <div>
-          <h1 style={{ textAlign: "center" }}>게시판</h1>
+          <h1 style={{ textAlign: "center" }} onClick={() => setPages(0)}>게시판</h1>
           <br></br>
           <button>내글보기</button>
 
@@ -76,26 +73,21 @@ function Logined(props: any): any {
           <div>
               <p><b>No 제목 작성일자 작성자 추천수 조회수</b></p>
             <br></br>
-              {/* <Page userName={userName} /> */}
           </div>
+          <Page params={{ pagenum: pages }} />
         </div>
-        
 
-        <Link href={`/board/pages/${param}`}>
-          Go to page
-        </Link>
-
-        {/* <Link href={`board/boardpage/${selectedBoard.bid}`}> */}
-          <button>이전페이지</button>
-        {/* </Link> */}
-        {/* <Link href="/view/$"> */}
-          <button>다음페이지</button>
-        {/* </Link> */}
+        <button onClick={() => setPages(pages-1)}>이전페이지</button>
+        {pages>=1 && <button onClick={() => setPages(pages-1)}>{pages-1}</button>}
+        {pages>1 && <button onClick={() => setPages(pages-2)}>{pages-2}</button>}
+        <p>{pages}</p>
+        <button onClick={() => setPages(pages+1)}>다음페이지</button>
+        <button onClick={() => setPages(pages+1)}>{pages+1}</button>
+        <button onClick={() => setPages(pages+2)}>{pages+2}</button>
 
         <div className="ui right floated button"
           onClick={ToggleAddForm}
-          style={{ position: "fixed", bottom: "5em", right: "5em" }}
-        >
+          style={{ position: "fixed", bottom: "5em", right: "5em" }}>
           게시글 작성하기
         </div>
         
@@ -107,7 +99,7 @@ function Logined(props: any): any {
             FormTitle="새로운 게시글 작성"
             handleXButton={handleAddXButton}
             formClass={AddFormClass}
-            // BoardComplete={CreateBoard}
+            BoardComplete={CreateBoard}
           />
         )}
       </div>
