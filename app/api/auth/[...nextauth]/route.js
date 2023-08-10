7938b004dec86f21d1c47ca0ad4032a61f36d930
rhquddus <rhquddus@naver.com>
2023-08-11 02:31:47 +0900

    google callback url

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,7 +11,7 @@ const nextAuthOptions = (req, res) => {
         clientId:
           "348049863995-rmkmgpkp5qvtiggpuc162e1hakjs5mo4.apps.googleusercontent.com",
         clientSecret: "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",
-        redirectUri: process.env.NEXT_PUBLIC_CALLBACKURL,
+        redirectUri: "https://luvoost.co.kr/api/auth/callback/google",
       }),
       KakaoProvider({
         clientId: "25edd438130f1d799655087b02557293",
