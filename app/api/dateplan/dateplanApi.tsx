4e9e666b24b804ed4ed845ae0413961561b59beb
jaiwon880 <rabbitmarry@gmail.com>
2023-08-11 16:11:04 +0900

    Dateplan

--- app/api/dateplan/dateplanApi.tsx
+++ app/api/dateplan/dateplanApi.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 const URL = process.env.NEXT_PUBLIC_URL;
-const API_URL = `${URL}/predict`; // Change with your actual server API URL
+const API_URL = `${URL}/predict`;
 
 export const predict = async (formData: any) => {
   const response = await axios.post(API_URL, JSON.stringify(formData), {
     headers: {
-      "Content-Type": "application/json; charset=utf-8",
+      'Content-Type': 'application/json; charset=utf-8',
     },
   });
   return response.data;
