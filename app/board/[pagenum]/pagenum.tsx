0e25afb6aabe309423ae3fe8f6179572e481022f
jaiwon880 <rabbitmarry@gmail.com>
2023-08-11 16:36:13 +0900

    Board

--- app/board/[pagenum]/pagenum.tsx
+++ app/board/[pagenum]/pagenum.tsx
@@ -1,10 +1,9 @@
-'use client'
-import React, { useState, useEffect, FC } from 'react';
-
-import Board from '../../../types/board';
-import SendData from '../../api/board/SendData';
-import WriteBoard from '../../../components/WriteBoard';
-import BoardDetail from '../../../components/BoardDetail';
+"use client";
+import React, { useState, useEffect, useCallback, FC } from "react";
+import Board from "../../../types/board";
+import SendData from "../../api/board/SendData";
+import WriteBoard from "../../../components/WriteBoard";
+import BoardDetail from "../../../components/BoardDetail";
 
 const defaultBoard: Board = {
   bid: 0,
@@ -18,135 +17,139 @@ const defaultBoard: Board = {
   b_recommendations: 0,
 };
 
-
-const board1:Board = {
-  bid: 3,
-  nickName: " dafsdf",
-  b_title: " weaff",
-  b_content: " wefasd",
-  b_createdAt: " fasdfas",
-  b_updatedAt: "",
-  b_views: 0,
-  comments: 0,
-  b_recommendations: 0,
-
+interface pageProps {
+  params: { pagenum: number };
 }
 
-
-const board2:Board = {
-  bid: 2,
-  nickName: " dafsdf",
-  b_title: " weaff",
-  b_content: " wefasd",
-  b_createdAt: " fasdfas",
-  b_updatedAt: "",
-  b_views: 0,
-  comments: 0,
-  b_recommendations: 0,
-
-}
-
-
-// const boards:Board[]=[board1,board2]
-
-interface pageProps{
-  params:{pagenum:number}
-}
-
-const Page:FC<pageProps> = ({params}, props:any) => {
-
-  const [boards, setBoards] = useState<Board[]|any>([]);
+const Page: FC<pageProps> = ({ params }, props: any) => {
+  const [boards, setBoards] = useState<Board[] | any>([]);
   const [UpdateFormClass, setUpdateFormClass] = useState<string | null>(null);
+  const [showUpdateForm, setShowUpdateForm] = useState<boolean>(false);
   const [selectedBoard, setSelectedBoard] = useState<Board>(defaultBoard); //선택된 board
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [commentListShow, setCommentListShow] = useState<boolean>(false);
 
-  const fetchData = async () => {
-    setIsLoading(true)
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
     try {
-      const response = await SendData("GET", `/api/boards/page/${params.pagenum}`,null,"fetch boards");
+      const response = await SendData(
+        "GET",
+        `/boards/page/${params.pagenum}`,
+        null,
+        "fetch boards"
+      );
       setBoards(response);
       setIsLoading(false);
-      console.log(response)
+      // console.log(response)
     } catch (error) {
       console.error("Error fetching boards:", error);
     }
-  };
+  }, [params.pagenum]);
   useEffect(() => {
     fetchData();
-  }, [params.pagenum]);
+  }, [fetchData]);
 
   // const boards=[board1,board2];
-  console.log(selectedBoard)
+  // console.log(selectedBoard)
 
   async function HandleBoardClick(event: any, clickedBoard: Board) {
-
     if (selectedBoard === null || selectedBoard.bid !== clickedBoard.bid) {
       setSelectedBoard(clickedBoard);
-      await SendData("GET", `/api/boards/${clickedBoard.bid}`,null,"view");
+      await SendData("GET", `/boards/${clickedBoard.bid}`, null, "view");
       fetchData();
       console.log(clickedBoard.bid + "번 게시글 조회됨");
-    } else if ( selectedBoard !== null && selectedBoard.bid === clickedBoard.bid ) {
+    } else if (
+      selectedBoard !== null &&
+      selectedBoard.bid === clickedBoard.bid
+    ) {
       setSelectedBoard(defaultBoard);
+      setCommentListShow(false);
     }
   }
   async function UpdateBoard(UpdateBoard: Board) {
     UpdateBoard.b_updatedAt = new Date().toISOString();
-    await SendData("PUT", `/api/boards/${UpdateBoard.bid}`,UpdateBoard,"update");
+    await SendData("PUT", `/boards/${UpdateBoard.bid}`, UpdateBoard, "update");
     setSelectedBoard(defaultBoard);
     setUpdateFormClass("formOff");
     fetchData();
   }
   async function DeleteBoard(board: Board) {
-    await SendData("DELETE", `/api/boards/${board.bid}`,board,"delete");
+    await SendData("DELETE", `/boards/${board.bid}`, board, "delete");
     fetchData();
   }
   function handelUpdateXButton() {
     setSelectedBoard(defaultBoard);
     setUpdateFormClass("formOff");
+    setTimeout(() => {
+      setShowUpdateForm(false);
+    }, 290);
   }
   function ToggleUpdateForm(board: Board) {
     setSelectedBoard((prevState) => {
       if (UpdateFormClass === "formOn") {
         setUpdateFormClass("formOff");
+        setTimeout(() => {
+          setShowUpdateForm(false);
+        }, 290);
         return defaultBoard;
       } else {
+        setShowUpdateForm(true);
         setUpdateFormClass("formOn");
         return board;
       }
     });
   }
-  async function HandleRecommendButton(bid:any) {
-    await SendData("PUT", `/api/boards/${bid}/recommend`,null,"recommend");
+  async function HandleRecommendButton(bid: any) {
+    await SendData("PUT", `/boards/${bid}/recommend`, null, "recommend");
     fetchData();
   }
-  
 
   return (
-    <div className="flex flex-col items-center space-y-4">
-      
-      {boards &&
-        boards.map((board:Board) => (
-          <React.Fragment key={board.bid}>
-            <div onClick={(event) => HandleBoardClick(event, board)} className="cursor-pointer w-full">
-              <div className={`w-96 bg-white shadow-md rounded p-4 font-bold ${selectedBoard && selectedBoard.bid === board.bid ? 'text-pink-500' : ''}`}>
-                <div >{board.bid} {board.b_title} {board.b_createdAt.toLocaleString()} {board.nickName} {board.b_recommendations.toLocaleString()} {board.b_views.toLocaleString()}</div>
+    <>
+      <div className="flex flex-col space-y-0">
+        {boards &&
+          boards.map((board: Board) => (
+            <React.Fragment key={board.bid}>
+              <div
+                onClick={(event) => HandleBoardClick(event, board)}
+                className={`cursor-pointer max-w-4xl w-full bg-white rounded-lg p-4 flex items-center justify-between ${
+                  selectedBoard && selectedBoard.bid === board.bid
+                    ? "text-pink-500"
+                    : ""
+                }`}
+              >
+                <div className="w-1/12 text-center">{board.bid}</div>
+                <div className="w-4/12 text-center">{board.b_title}</div>
+                <div className="w-2/12 text-center">
+                  {board.b_createdAt.toLocaleString()}
+                </div>
+                <div className="w-2/12 text-center">{board.nickName}</div>
+                <div className="w-1/12 text-center">
+                  {board.b_recommendations}
+                </div>
+                <div className="w-1/12 text-center">{board.b_views}</div>
               </div>
-            </div>
-  
-            <br></br>
-            <div className={`transition ${selectedBoard && selectedBoard.bid === board.bid ? "max-h-screen" : "max-h-0"}`}>
-              {selectedBoard && selectedBoard.bid === board.bid && <BoardDetail
-                userName={props.userName}
-                selectedBoard={board}
-                ToggleUpdateForm={() => ToggleUpdateForm(board)}
-                DeleteBoard={DeleteBoard}
-                HandleRecommendButton={HandleRecommendButton}
-              />}
-            </div>
-          </React.Fragment>
-        ))}
-  
-      {UpdateFormClass && (
+              <div
+                className={`board-detail flex ${
+                  selectedBoard && selectedBoard.bid === board.bid
+                    ? "active"
+                    : ""
+                }`}
+              >
+                <BoardDetail
+                  userName={props.userName}
+                  selectedBoard={board}
+                  ToggleUpdateForm={() => ToggleUpdateForm(board)}
+                  DeleteBoard={DeleteBoard}
+                  HandleRecommendButton={HandleRecommendButton}
+                  commentListShow={false}
+                  setCommentListShow={setCommentListShow}
+                />
+              </div>
+            </React.Fragment>
+          ))}
+      </div>
+      {UpdateFormClass && showUpdateForm && (
         <WriteBoard
           board={{ ...selectedBoard }}
           FormTitle="게시글 수정"
@@ -155,9 +158,8 @@ const Page:FC<pageProps> = ({params}, props:any) => {
           BoardComplete={UpdateBoard}
         />
       )}
-    </div>
-
+    </>
   );
-}
+};
 
 export default Page;
