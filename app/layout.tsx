403e89963846d8f6248fcc5aa5eb1e8602ed832c
jaiwon880 <rabbitmarry@gmail.com>
2023-08-21 17:40:57 +0900

    [Top] 로그인 버튼이 중첩되어 발생하는 오류를 span 태그로 정보를 렌더링 하는 방법으로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from "react";
 import AuthSession from "./AuthSession";
-import Top from "../components/Top";
+import MenuListCompotision from "./../components/MenuDrop";
 import Footer from "../components/Footer";
 import { Provider as ReduxProvider } from "react-redux";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 import { store } from "../store";
+// import Top from "../components/Top";
 
 export default function RootLayout({
   children,
@@ -14,15 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <GoogleAnalytics trackingId="G-FDVK366KNX" />
+      {/* <GoogleAnalytics trackingId="G-FDVK366KNX" /> */}
       <body>
         <ReduxProvider store={store}>
           <AuthSession>
-          <Top />
-            <div className="mx-auto max-w-4xl">
-              {children}
-              <Footer />
-            </div>
+            {/* <Top /> */}
+            <MenuListCompotision />
+            <div className="mx-auto max-w-4xl">{children}</div>
+            <Footer />
           </AuthSession>
         </ReduxProvider>
       </body>
