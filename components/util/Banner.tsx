411184e5e4a669ae275f991e4b0c53ec55d97327
jaiwon880 <rabbitmarry@gmail.com>
2023-08-26 17:39:08 +0900

    [Main - Banner] 메인페이지에서 발생하는 경고문 제거

--- components/util/Banner.tsx
+++ components/util/Banner.tsx
@@ -1,44 +1,21 @@
 "use client";
-import React from "react";
 import Lottie from "react-lottie-player";
 import lottieJson from "../../public/animations/main.json";
-import ArrowRight from "./ArrowRight";
+import Image from "next/image";
 import Link from "next/link";
 import "./../../public/css/banner.css";
-import Image from "next/image";
 
 export default function Banner({
 }) {
-  // hover와 관련된 상태 및 함수 삭제
-  // const [isHovered, setIsHovered] = useState(false);
-
-  // const handleHover = () => {
-  //     setIsHovered(true);
-  // };
-
-  // const handleLeave = () => {
-  //     setIsHovered(false);
-  // };
-
-  // const animationStyle = {
-  //   ...style,
-  //   transition: "width 0.3s, height 0.3s",
-  //   // hover와 관련된 스타일 수정 삭제
-  //   // width: isHovered ? style.width * 1.2 : style.width,
-  //   // height: isHovered ? style.height * 1.2 : style.height,
-  // };
 
   return (
     <div
       className="flex items-center"
-      // onMouseEnter와 onMouseLeave 핸들러 삭제
-      // onMouseEnter={handleHover}
-      // onMouseLeave={handleLeave}
       style={{ fontFamily: "Chosunilbo_myungjo" }}
     >
-      <Image src="/image/main.gif" alt="mainBanner" width={400} height={380} />
-      {/* <Lottie animationData={lottieJson}/> */}
-      <div className="ml-4 slide-in-right">
+      <Image src="/image/main.gif" alt="mainBanner" width={300} height={280}/>
+      {/* <Lottie animationData={lottieJson} play={play} style={animationStyle} /> */}
+      <div className="ml-10 slide-in-right">
         <p className="text-3xl font-bold leading-tight">
           데이트 코스 걱정 끝
           <br />내 연인과의 추억을 쌓는 곳
@@ -50,7 +27,6 @@ export default function Banner({
             passHref
           >
             <span className="animate-arrow">코스 추천 받으러 가기</span>
-            {/* <ArrowRight /> */}
           </Link>
         </div>
       </div>
