47345548a6a83abd9479358ee47c1721f9fe80e1
mastgm0817 <mastgm0817@gmail.com>
2023-08-28 04:19:17 +0900

    arror right

--- components/util/ArrowRight.tsx
+++ components/util/ArrowRight.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Lottie from "react-lottie-player";
-import lottieJson from "../../public/animations/arrowRight.json";
+import lottieJson from "../../public/animations/lover.json";
 export default function ArrowRight() {
   return (
-    
     <Lottie
       loop
       animationData={lottieJson}
