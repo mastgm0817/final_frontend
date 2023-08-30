7437e795443e86549d2c567c7f41c56d20175f31
whataLIN <kitschh@naver.com>
2023-08-30 15:07:12 +0900

    payment design

--- app/price/page.js
+++ app/price/page.js
@@ -1,19 +1,8 @@
 "use client";
 import * as React from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from "@mui/material/CardHeader";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import StarIcon from "@mui/icons-material/StarBorder";
-import Typography from "@mui/material/Typography";
-import GlobalStyles from "@mui/material/GlobalStyles";
-import Container from "@mui/material/Container";
 import { usePathname, useRouter } from "next/navigation";
+import "./../../public/css/fonts.css";
+import "./../../public/css/coupon.css";
 
 const tiers = [
   {
@@ -25,8 +14,9 @@ const tiers = [
       "좋을껄?",
       "계속 서비스 추가될거야",
     ],
-    buttonText: "결제하기",
-    buttonVariant: "outlined",
+    buttonText: "구매하기",
+    buttonVariant: "bg-rose-500",
+    backgroundC:"rose-400",
   },
   {
     title: "한달 이용권",
@@ -37,8 +27,9 @@ const tiers = [
       "아마 좋을껄?",
       "계속 서비스 추가될거야",
     ],
-    buttonText: "결제하기",
-    buttonVariant: "contained",
+    buttonText: "구매하기",
+    buttonVariant: "bg-rose-600",
+    backgroundC:"rose-500",
   },
   {
     title: "일년 이용권",
@@ -49,13 +40,12 @@ const tiers = [
       "후후후후 제발",
       "계속 서비스 추가될거야",
     ],
-    buttonText: "결제하기",
-    buttonVariant: "outlined",
+    buttonText: "구매하기",
+    buttonVariant: "bg-rose-700",
+    backgroundC:"rose-600"
   },
 ];
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
 
 export default function Pricing() {
   const pathname = usePathname();
@@ -74,111 +64,63 @@ export default function Pricing() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles
-        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
-      />
-      <CssBaseline />
+    <div className="container mx-auto px-4">
 
-      <Container
-        disableGutters
-        maxWidth="sm"
-        component="main"
-        sx={{ pt: 8, pb: 6 }}
-      >
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="text.primary"
-          gutterBottom
-        >
-          VIP 정기권 구매
-        </Typography>
-        <Typography
-          variant="h5"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          싸다싸
-        </Typography>
-      </Container>
-      {/* End hero unit */}
-      <Container maxWidth="md" component="main">
-        <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid
-              item
-              key={tier.title}
-              xs={12}
-              sm={tier.title === "Enterprise" ? 12 : 6}
-              md={4}
-            >
-              <Card>
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: "center" }}
-                  action={tier.title === "Pro" ? <StarIcon /> : null}
-                  subheaderTypographyProps={{
-                    align: "center",
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === "light"
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "baseline",
-                      mb: 2,
-                    }}
-                  >
-                    <Typography
-                      component="h2"
-                      variant="h3"
-                      color="text.primary"
-                    >
-                      {tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="text.secondary">
-                      원
-                    </Typography>
-                  </Box>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography
-                        component="li"
-                        variant="subtitle1"
-                        align="center"
-                        key={line}
-                      >
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    fullWidth
-                    variant={tier.buttonVariant}
-                    onClick={() => goToPaymentPage(tier)} // 버튼을 클릭하면 선택한 상품 정보를 넘김
-                  >
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </ThemeProvider>
+      <br/><br/>
+      <div className="text-left my-8">
+        <h1 className="text-4xl font-bold">VIP 정기권 구매</h1>
+      </div>
+      <span><hr/></span><br/>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+
+        {tiers.map((tier) => (
+          <div key={tier.title} className="rounded-lg overflow-hidden bg-gray-50 transform transition-transform duration-300 hover:scale-105">
+            <div className={`px-6 py-4 bg-${tier.backgroundC}`}>
+              <div className="font-extrabold text-2xl text-white text-center my-5 mb-7">{tier.title}</div> 
+            
+            <div className="bg-white border rounded-lg">
+            <div className="px-6 py-4">
+              <div className="font-extrabold text-4xl m-4 mb-7 text-center">{tier.price}<span className="text-lg ml-2">원</span></div>
+              <hr /><br />
+              <ul className="list-disc pl-5">
+                {tier.description.map((line) => (
+                  <li key={line} className="mb-2">{line}</li>
+                ))}
+              </ul>
+            </div>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <button
+                className={`w-full py-2 px-3 text-lg font-bold text-rose-700  text-white rounded-lg border-${tier.buttonVariant} bg-${tier.backgroundC} hover:${tier.buttonVariant}`}
+                onClick={() => goToPaymentPage(tier)}
+              >
+                {tier.buttonText}
+              </button>
+              
+            </div>
+            </div>
+          </div>
+        ))}
+      
+      </div>
+
+      <div className="container mx-auto px-4">
+        <br /><br /><br />
+        <div className="font-bold my-2">이용권 구매 시 유의사항</div>
+
+        <div className="ml-2 mb-20">
+          <ol>
+            <li>1주 이용권은 결제일로부터 1주일의 사용 기간을 기준으로 하는 1주 단위 상품으로, 구매 시간으로부터 1주일 경과 시 자동 소멸됩니다.</li>
+            <li>1개월 이용권은 결제일로부터 1개월의 사용 기간을 기준으로 하는 1개월 단위 상품으로, 구매 시간으로부터 1개월 경과 시 자동 소멸됩니다.</li>
+            <li>1년 이용권은 결제일로부터 1년의 사용기간을 기준으로 하는 1개월 단위 상품으로, 구매 시간으로부터 1년 경과 시 자동 소멸됩니다.</li>
+            <li>이용 기간 만료 후에 자동 연장되지 않으며, 추가 이용을 원하실 경우 재결제가 필요합니다.</li>
+            <li>결제 시 별도의 부가세를 부과하지 않습니다.</li>
+            <li>그 외 이용권 구매 관련 문의사항은 상단의 INQUIRY를 통해 문의해주세요.</li>
+          </ol>
+        </div>
+      </div>
+    </div>
   );
 }
+
